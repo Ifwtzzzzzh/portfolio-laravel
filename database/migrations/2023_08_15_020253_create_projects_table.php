3ece84bcad85ed35f9c9270bde340cf475df1171
NosaR <rahmandanosa@gmail.com>
2023-08-15 10:42:36 +0700

    Database setup

---
+++ database/migrations/2023_08_15_020253_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('category');
+            $table->string('tech_stack');
+            $table->string('image');
+            $table->text('description');
+            $table->string('project_link')->nullable();
+            $table->string('doc_link');
+            $table->string('github_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
